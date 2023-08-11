41419a8f21911e4bea58f3eb96a221cce60a5bd2
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-12 00:10:37 +0530

    setup

--- app.js
+++ app.js
@@ -1,61 +1,95 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const exp = require("constants");
-const { log } = require("console");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 const app = express();
+
+console.log(process.env.API_KEY);
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-main().catch((err) => console.log(err));
+mongoose.connect("mongodb://localhost:27017/userDB").then(() => {
+  console.log("connected to DB");
+});
 
-async function main() {
-  // connect mongo
-  await mongoose.connect("mongodb://localhost:27017/userDB").then(() => {
-    console.log("connected to mongo");
-  });
-}
-// schema declared
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
-// model created
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
 const User = new mongoose.model("User", userSchema);
-app.get("/", (req, res) => {
+
+app.get("/", function (req, res) {
   res.render("home");
 });
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/login", (req, res) => {
+
+app.get("/login", function (req, res) {
   res.render("login");
 });
-app.get("/register", (req, res) => {
+
+app.get("/register", function (req, res) {
   res.render("register");
 });
-// creating new user
-app.post("/register", (req, res) => {
+
+app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password,
   });
-  newUser.save().then((err) => {
-    if (err) {
-      console.log("new user created");
-
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
-  });
+  newUser.save();
+  res.render("secrets");
+  // newUser.save(function (err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     res.render("secrets");
+  //   }
+  // });
 });
 
-// listening on port 3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log("connected to port 3000");
+app.post("/login", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  await User.findOne({ email: username }).then(function (foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+
+  // User.findOne({ email: username }, function (err, foundUser) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       if (foundUser.password === password) {
+  //         res.render("secrets");
+  //       }
+  //     }
+  //   }
+  // });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
 });
